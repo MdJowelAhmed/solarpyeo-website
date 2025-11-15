596ceec537a70ef2b8611a7465bed2e4eca06f65
Md Jowel Ahmed <155435567+MdJowelAhmed@users.noreply.github.com>
2025-11-15 11:03:29 +0600

    update base url

--- src/components/share/imageUrl.js
+++ src/components/share/imageUrl.js
@@ -6,8 +6,8 @@ export const getImageUrl = (path) => {
   if (path.startsWith("http://") || path.startsWith("https://")) {
     return path;
   } else {
-    const baseUrl = "http://10.10.7.46:5000";
-    // const baseUrl = "https://api.yogawithjen.life";
+    // const baseUrl = "http://10.10.7.46:5000";
+    const baseUrl = "http://206.162.244.188:5000";
     return `${baseUrl}/${path}`;
   }
 };
